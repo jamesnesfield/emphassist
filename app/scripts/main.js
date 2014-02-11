6cb09246a6aa99fb1baa9a3f6e2ea8e80b105ccb
James Nesfield <james@nesfield.org>
2014-02-11 23:49:18 +0000

    rc1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,30 +9,45 @@ function search() {
 		console.log( $('#search').cleanHtml());
 
 
-		var fullInputText = $( '#search').cleanHtml();
+		var fullInputText = $( '#search').text();
+		var plainText = $("#search")
+        .clone()    //clone the element
+        .children() //select all the children
+        .remove()   //remove all the children
+        .end()  //again go back to selected element
+        .text();    //get the text of element
 		var boldItems     = $( '#search' ).find( 'b' ).toArray();
 		var italicItems   = $( '#search' ).find( 'i' ).toArray();
 		var strikeItems   = $( '#search' ).find( 'strike' ).toArray();
 
+		var search = '';
+
+		console.log('strikethrough items : ');
+		for (var i = strikeItems.length - 1; i >= 0; i--) {
+			console.log(i+' '+strikeItems[i].innerText.toString());
+			search += '+-'+strikeItems[i].innerText.toString();
+
+		};
+
 		console.log('bold items : ');
 		for (var i = boldItems.length - 1; i >= 0; i--) {
-			console.log(i+' '+boldItems[i].innerText);
+			console.log(i+' '+boldItems[i].innerText.toString());
+			search += '+'+boldItems[i].innerText.toString()+' '+boldItems[i].innerText.toString();
 		};
 
 		console.log('italic items : ');
 		for (var i = italicItems.length - 1; i >= 0; i--) {
-			console.log(i+' '+italicItems[i].innerText);
+			console.log(i+' '+italicItems[i].innerText.toString());
 		};
 
-		console.log('strikethrough items : ');
-		for (var i = strikeItems.length - 1; i >= 0; i--) {
-			console.log(i+' '+strikeItems[i].innerText);
-		};
+	
 
-		var search = boldItems.join(' ')+boldItems.join(' ');
-
-		console.log('search : '+search);
-		//window.location = 'http://www.google.com';
+		// 'https://encrypted.google.com/search?'+boldItems.join(' ')+boldItems.join(' ');
+		console.log('text : '+fullInputText);
+		console.log('plain : '+plainText);	
+		var searchTerm = 'https://encrypted.google.com/search?q='+plainText+'+'+search;	
+		console.log('search : '+searchTerm);
+		window.location = searchTerm;
 	}
 }
 
